11b83e03a40d9e9bd995b37829682a16540d0619
Rafi <rafi.bogra@gmail.com>
2017-02-09 11:07:06 +0000

    server port reset

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 var http = require('http');
 var path = require('path');
-
+var debug = require('debug')('gmap-road:server');
 var async = require('async');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -15,7 +15,7 @@ var models = require('./models/models');
 var gpsApi = require("./routes/gps.route");
 
 var app = express();
-var server = http.createServer(app);
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,8 +27,42 @@ app.use('/api', gpsApi);
 app.use(express.static(path.resolve(__dirname, 'client')));
 
 
+// var server = http.createServer(app);
+// server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+//   var addr = server.address();
+//   console.log("server listening at", addr.address + ":" + addr.port);
+// });
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("server listening at", addr.address + ":" + addr.port);
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+var server = http.createServer(app);
+
+server.listen(port, function() {
+  console.log("server listening");
 });
+server.on('listening', onListening);
+
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
